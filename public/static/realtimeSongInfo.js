7467c33bd956458a05f5b56585632497cd7a86e8
Léo Lam <leo@innovatetechnologi.es>
2015-07-18 17:23:19 +0800

    Public: Prevent potential XSS by using textContent

--- public/static/realtimeSongInfo.js
+++ public/static/realtimeSongInfo.js
@@ -3,16 +3,16 @@
 var socket = io();
 socket.on('metadata', function onMetadataUpdate (msg) {
     if (document.getElementById('meta-' + msg.stream) !== null) {
-        document.getElementById('meta-' + msg.stream).innerHTML = msg.song;
+        document.getElementById('meta-' + msg.stream).textContent = msg.song;
     }
 });
 
 socket.on('listenerCountChange', function onListenerCountChange (msg) {
     if (document.getElementById('listencount-' + msg.stream) !== null) {
         if (msg.count === 1) {
-            document.getElementById('listencount-'+msg.stream).innerHTML = '1 person is listening right now.';
+            document.getElementById('listencount-'+msg.stream).textContent = '1 person is listening right now.';
         } else {
-            document.getElementById('listencount-'+msg.stream).innerHTML = msg.count + ' people are listening right now.';
+            document.getElementById('listencount-'+msg.stream).textContent = msg.count + ' people are listening right now.';
         }
     }
 });
