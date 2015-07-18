4ca268d2dddd33f2173b8d59c640b532b3a08bf0
Léo Lam <leo@innovatetechnologi.es>
2015-07-18 17:24:26 +0800

    Public: Reduce code duplication in realtimeSongInfo

--- public/static/realtimeSongInfo.js
+++ public/static/realtimeSongInfo.js
@@ -2,17 +2,19 @@
 
 var socket = io();
 socket.on('metadata', function onMetadataUpdate (msg) {
-    if (document.getElementById('meta-' + msg.stream) !== null) {
-        document.getElementById('meta-' + msg.stream).textContent = msg.song;
+    var metadataElement = document.getElementById('meta-' + msg.stream);
+    if (metadataElement !== null) {
+        metadataElement.textContent = msg.song;
     }
 });
 
 socket.on('listenerCountChange', function onListenerCountChange (msg) {
-    if (document.getElementById('listencount-' + msg.stream) !== null) {
+    var countElement = document.getElementById('listencount-' + msg.stream);
+    if (countElement !== null) {
         if (msg.count === 1) {
-            document.getElementById('listencount-'+msg.stream).textContent = '1 person is listening right now.';
+            countElement.textContent = '1 person is listening right now.';
         } else {
-            document.getElementById('listencount-'+msg.stream).textContent = msg.count + ' people are listening right now.';
+            countElement.textContent = msg.count + ' people are listening right now.';
         }
     }
 });
