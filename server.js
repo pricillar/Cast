3faca3d9cd66ed3cd818453c907e23b6d8a38d28
Léo Lam <leo@innovatetechnologi.es>
2015-07-28 22:18:12 +0800

    Clean up server.js

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
-var colors = require('colors'),
-    wait = require("wait.for")
+require('colors');
+var wait = require('wait.for');
 
-console.log("Cast 1.0 ".rainbow + "Beta 1".inverse);
-global.localdir = __dirname
+console.log('Cast 1.0 '.rainbow + 'Beta 1'.bold);
+
+global.localdir = __dirname;
 global.cast = {
-        version: '1.0'
-    } //defining build specific info
+    version: '1.0'
+}; // defining build specific info
 
 var fs = require('fs');
 global.config = JSON.parse(fs.readFileSync(global.localdir + '/config.json', 'utf8'));
 
-var loadCast = function() {
-    global.app = require("./intern/HTTP/server.js");
-    global.hooks = require("./hooks/hooks.js")
-    global.hooks.loadModules()
+var loadCast = function loadCast () {
+    global.app = require('./intern/HTTP/server.js');
+    global.hooks = require('./hooks/hooks.js');
+    global.hooks.loadModules();
+};
+
+if (global.config.startUpScript) { // run a script before going on eg. to load the configuration from a network disk
+    wait.launchFiber(function () {
+        var start = require(global.localdir + '/start.js');
+        wait.for(start);
+        loadCast();
+    });
+} else {
+    loadCast();
 }
-
-
-if (global.config.startUpScript) { //run a script before going on eg. to load the configuration from a network disk
-    wait.launchFiber(function() {
-        var start = require(global.localdir + "/start.js")
-        wait.for(start)
-        loadCast()
-    })
-}else{
-    loadCast()
-}
-
-
