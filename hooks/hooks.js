1e463abda92ba96c06481e3dda0ea0f8bcee5b0c
Maartje Eyskens <meyskens@me.com>
2016-08-21 13:53:10 +0200

    Only load in JS files

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -6,7 +6,7 @@ global.events = new Events();
 
 const actionModules = fs.readdirSync(global.localdir + "/hooks/action");
 for (let module of actionModules) {
-    if (fs.statSync(`${global.localdir}/hooks/action/${module}`).isFile()) {
+    if (fs.statSync(`${global.localdir}/hooks/action/${module}`).isFile() && module.split(".")[module.split(".").length -1] === "js") {
         require(`${global.localdir}/hooks/action/${module}`);
     }
 }
