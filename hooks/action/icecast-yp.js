a07a03016af4668503f32ea8b840911705260c6f
Maartje Eyskens <meyskens@me.com>
2016-12-16 11:53:28 +0100

    Hard code ports

--- hooks/action/icecast-yp.js
+++ hooks/action/icecast-yp.js
@@ -26,9 +26,9 @@ let addToDir = (stream) => {
     if (config.hostname.split(":").length === 3) { // if a non standard port is used
         hostname = config.hostname;
     } else if (config.httpPort === 0) { // if only serves over https
-        hostname = config.hostname + ":" + config.httpsPort.toString()
+        hostname = config.hostname + ":443"
     } else { // If both or only http, use http as some directories may have https issues
-        hostname = config.hostname.replace("https://", "http://") + ":" + config.httpPort.toString()
+        hostname = config.hostname.replace("https://", "http://") + ":80"
     }
 
     var sendRequestToDirectory = (directory) => {
