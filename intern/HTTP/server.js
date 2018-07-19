48a7322c75a1afcad11b0f8e71d2f4641e302662
Maartje Eyskens <meyskens@me.com>
2018-07-19 22:04:13 +0200

    fix double match

--- intern/HTTP/server.js
+++ intern/HTTP/server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
-    if (req.ip && req.ip.match.match("^::ffff:")) {
+    if (req.ip && req.ip.match("^::ffff:")) {
         req.ip = req.ip.replace("::ffff:", "")
     }
     next();
