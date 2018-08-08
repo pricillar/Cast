2e8d27a546041d0081d32a03c76d65a5df1bd9de
Maartje Eyskens <maartje@eyskens.me>
2018-08-08 17:07:19 +0200

    MPAG DASH (#113)
    * change HLS header to application/vnd.apple.mpegurl
    
    * Add experimental HLS using ffmpeg
    
    * install ffmpeg in the container
    
    * rm consoe log
    
    * clean up hls folder
    
    * use .send()
    
    * Send out name inside TS
    
    * add Cache-Control
    
    * Add experimental DASH support
    
    * add serveMPD
    
    * improve whitespace
    
    * fix removeStream
    
    * use streamName
    
    * use rimraf
    
    * keep 20 segments
    
    * add rimaf callback
    
    * save file actually....
    
    * clearInterval on stop
    
    * fix typo in DASHHandler
    
    * fix pasting in the wrong window
    
    * retry fetch of playlist
    
    * log failings of get file
    
    * fix infinite loop
    
    * keep 7 fragments in list, 20 on disk for hls

---
+++ intern/streams/dash.js
@@ -0,0 +1,96 @@
+import fs from "fs"
+import util from "util"
+import tmp from "tmp"
+import rimraf from "rimraf"
+import { spawn } from "child_process"
+
+const unlink = util.promisify(fs.unlink)
+const readdir = util.promisify(fs.readdir)
+
+const KEEP_SEGMENTS = 20
+const chunkRegex = new RegExp(/chunk-stream0-(.*).m4s/)
+
+export default class DASHHandler {
+
+    tempPath = ""
+    tmpobj
+    inputStream
+    process
+    name = ""
+
+    oldestChunk = 1
+    chunkTimer
+
+    constructor(inputStream, name) {
+        this.tmpobj = tmp.dirSync();
+        this.tempPath = this.tmpobj.name
+
+        this.inputStream = inputStream
+        this.name = name
+    }
+
+    /*
+        We use ffmpeg as the best way to implement this atm.
+        Transmuxing in JS will take more resources and currently there is no good library available
+        In order to move fast we decided to let ffmpeg do the job here in a subprocess.
+    */
+
+    start() {
+        this.process = spawn('ffmpeg', [
+            "-y",
+            "-i", "-",
+            "-codec", "copy",
+            "-live", "1",
+            "-use_timeline", `0`,
+            "-media_seg_name", "chunk-stream$RepresentationID$-$Number$.m4s",
+            "-remove_at_exit", "1",
+            "-f", "dash",
+            "-metadata", `title=${this.name}`,
+            `${this.tempPath}/dash.mpd`
+        ],
+        { 
+            stdio: ['pipe', null, null]
+        });
+    
+        this.inputStream.pipe(this.process.stdin);
+        this.chunkTimer = setInterval(this.deleteOldChunks.bind(this), 10000)
+    }
+
+    stop() {
+        this.process.kill()
+        clearInterval(this.chunkTimer)
+        rimraf(this.tempPath, () => {})
+    }
+
+    async deleteOldChunks() {
+        const files = await readdir(this.tempPath)
+        const chunkList = []
+
+        let highest = 0
+        let lowest = Infinity
+
+        for (let file of files) {
+            if (file.match(chunkRegex)) {
+                chunkList.push(file)
+            }
+        }
+
+        for (let chunk of chunkList) {
+            const match = chunkRegex.exec(chunk);
+            const number = parseInt(match[1], 10);
+            if (number > highest) {
+                highest = number
+            }
+            if (number < lowest) {
+                lowest = number
+            }
+        }
+        
+        if (highest - KEEP_SEGMENTS > lowest) {
+            this.oldestChunk = (highest - KEEP_SEGMENTS )
+            for (let i = lowest; i < (highest - KEEP_SEGMENTS ); i++) {
+                await unlink(`${this.tempPath}/chunk-stream0-${i}.m4s`)
+            }
+        }
+    }
+}
